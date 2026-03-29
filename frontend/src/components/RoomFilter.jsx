e5718ad4cbaa2f6ba29182e28351054db1d0fcd8
Vasu Goel <vasugoel02909@gmail.com>
2026-03-29 20:20:51 +0530

    chore: add dotenv dependency to backend and remove unused useState hook from RoomFilter component

--- frontend/src/components/RoomFilter.jsx
+++ frontend/src/components/RoomFilter.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { Filter, Wind, Monitor, Search, Layers, Check } from 'lucide-react';
 import { AppContext } from '../context/AppContext';
 
